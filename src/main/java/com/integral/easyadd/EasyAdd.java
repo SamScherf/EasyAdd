1978082d055dd700fa03db830949a77af4943fbc
Sam Scherf <github@samscherf.com>
2022-12-09 05:53:02 -0800

    Clean up main file

--- src/main/java/com/integral/easyadd/EasyAdd.java
+++ src/main/java/com/integral/easyadd/EasyAdd.java
@@ -1,26 +1,10 @@
 package com.integral.easyadd;
 
 import net.minecraft.creativetab.CreativeTabs;
-import net.minecraft.init.Blocks;
 import net.minecraft.init.Items;
 import net.minecraft.item.Item;
-import net.minecraftforge.common.MinecraftForge;
 
-import java.io.File;
-
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-
-import cpw.mods.fml.common.FMLCommonHandler;
 import cpw.mods.fml.common.Mod;
-import cpw.mods.fml.common.Mod.EventHandler;
-import cpw.mods.fml.common.Mod.Instance;
-import cpw.mods.fml.common.SidedProxy;
-import cpw.mods.fml.common.event.FMLInitializationEvent;
-import cpw.mods.fml.common.event.FMLPreInitializationEvent;
-import cpw.mods.fml.common.event.FMLServerStartingEvent;
-import cpw.mods.fml.common.network.NetworkRegistry;
-import cpw.mods.fml.common.network.simpleimpl.SimpleNetworkWrapper;
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
 
@@ -30,7 +14,7 @@ public class EasyAdd {
     public static final String NAME = "Easy Add";
     public static final String VERSION = "@VERSION@";
 
-    public static CreativeTabs tabExampleMod = new CreativeTabs("tabExampleMod") {
+    public static CreativeTabs tabEasyAdd = new CreativeTabs("EasyAdd") {
         @Override
         @SideOnly(Side.CLIENT)
         public Item getTabIconItem() {
