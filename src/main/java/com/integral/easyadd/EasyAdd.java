65d0482f0f8ccba2062844045c83c764b8566377
Sam Scherf <github@samscherf.com>
2022-12-09 05:48:15 -0800

    Remove unnecessary template features

--- src/main/java/com/integral/easyadd/EasyAdd.java
+++ src/main/java/com/integral/easyadd/EasyAdd.java
@@ -11,11 +11,6 @@ import java.io.File;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import com.integral.easyadd.handlers.ExampleConfig;
-import com.integral.easyadd.handlers.GenericEventHandler;
-import com.integral.easyadd.network.packets.ExamplePacket;
-import com.integral.easyadd.proxy.CommonProxy;
-
 import cpw.mods.fml.common.FMLCommonHandler;
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.Mod.EventHandler;
@@ -34,39 +29,6 @@ public class EasyAdd {
     public static final String MODID = "easyadd";
     public static final String NAME = "Easy Add";
     public static final String VERSION = "@VERSION@";
-
-    public static SimpleNetworkWrapper packetHandler;
-
-    @SidedProxy(clientSide = "com.integral.easyadd.proxy.ClientProxy", serverSide = "com.integral.easyadd.proxy.CommonProxy")
-    public static CommonProxy proxy;
-
-    public static final Logger logger = LogManager.getLogger("EasyAdd");
-    public static final int howCoolAmI = Integer.MAX_VALUE;
-
-    @EventHandler
-    public void load(FMLInitializationEvent event) {
-        proxy.registerRenderers();
-    }
-
-    @EventHandler
-    public void serverLoad(FMLServerStartingEvent event) {
-        // NO-OP
-    }
-
-    @EventHandler
-    public void preInit(FMLPreInitializationEvent event) {
-        ExampleConfig.load(event);
-
-        packetHandler = NetworkRegistry.INSTANCE.newSimpleChannel("ExampleModChannel");
-        packetHandler.registerMessage(ExamplePacket.Handler.class, ExamplePacket.class, 1, Side.CLIENT);
-
-        GenericEventHandler handler = new GenericEventHandler();
-        MinecraftForge.EVENT_BUS.register(handler);
-        FMLCommonHandler.instance().bus().register(handler);
-
-        NetworkRegistry.INSTANCE.registerGuiHandler(this, proxy);
-    }
-
 
     public static CreativeTabs tabExampleMod = new CreativeTabs("tabExampleMod") {
         @Override
