0ae481864882f91d9e560ad3b44c2c4b16f2b9a9
Sam Scherf <github@samscherf.com>
2022-12-09 07:01:33 -0800

    Create addItem method to streamline adding items

--- src/main/java/com/integral/easyadd/EasyAddItems.java
+++ src/main/java/com/integral/easyadd/EasyAddItems.java
@@ -7,10 +7,22 @@ import com.integral.easyadd.EasyAdd;
 
 public class EasyAddItems
 {
-    public static void initItems()
-    {
-	Item mario = new Item().setUnlocalizedName("mario").setTextureName(EasyAdd.MODID + ":mario");
-        GameRegistry.registerItem(mario, "mario");
-	mario.setCreativeTab(EasyAdd.tabEasyAdd);
+    public static Item addItem(String name) {
+	    // Create new item
+	    Item newItem = new Item();
+	    
+	    // Set name based attributes
+	    newItem.setUnlocalizedName(name);
+	    newItem.setTextureName(EasyAdd.MODID + ":" + name);
+	    newItem.setCreativeTab(EasyAdd.tabEasyAdd);
+
+	    // Register and return
+            GameRegistry.registerItem(newItem, name);
+	    return newItem;
+    }
+
+    public static void initItems() {
+	    // Add your items here by repeatedly calling addItem
+	    addItem("mario");
     }
 }
